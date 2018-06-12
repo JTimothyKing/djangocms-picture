d3e5bddbbcaf180c77b636b4db0800d30d5f2911
Tim King <timk@jtimothyking.com>
2018-06-12 08:00:27 -0400

    Hack override assertHTMLEqual() in order to properly detect whitespace errors.
    This code does NOT work with Python 2. Python 2 support still
    needs to be added.

--- tests/tests_cms_plugins.py
+++ tests/tests_cms_plugins.py
@@ -8,10 +8,57 @@ from cms.plugin_rendering import ContentRenderer
 
 from djangocms_picture.cms_plugins import PicturePlugin
 
+from django.utils.html_parser import HTMLParser
+
+import re
+
+
+class _NormalizedHTML(HTMLParser):
+    def __init__(self, html):
+        HTMLParser.__init__(self)
+        self.segments = []
+        self.feed(html)
+        self.close()
+
+    def handle_starttag(self, tag, attrs):
+        normalized_attrs = [
+            (name, " ".join(sorted(value.split(" "))))
+            if name == "class"
+            else (name, value)
+            for name, value in sorted(attrs)
+        ]
+        attr_str = " ".join([
+            '%s="%s"' % (name, value)
+            if value
+            else "%s" % name
+            for name, value in normalized_attrs
+        ])
+        tag_str = "<%s %s>" % (tag, attr_str)
+        self.segments.append(tag_str)
+
+    def handle_endtag(self, tag):
+        tag_str = "</%s>" % tag
+        self.segments.append(tag_str)
+
+    def handle_data(self, data):
+        normalized_data = re.sub(r'\s+', " ", data)
+        self.segments.append(normalized_data)
+
+    def __str__(self):
+        return ("".join(self.segments)).strip()
+
+def _normalized(html):
+    normalized_html = _NormalizedHTML(html)
+    return str(normalized_html)
+
 
 class PicturePluginTestCase(TestCase):
     def setUp(self):
         self.renderer = ContentRenderer(request=RequestFactory())
+
+    # Override with our version.
+    def assertHTMLEqual(self, html1, html2, msg=None):
+        self.assertEqual(_normalized(html1), _normalized(html2), msg)
 
     def _create_instance(self, **data):
         placeholder = Placeholder.objects.create(slot='test')
